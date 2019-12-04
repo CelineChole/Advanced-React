1ced106d410e6535648588baead56281515cca62
celinechole <github@celinechole.com>
2019-12-04 09:27:46 -0500

    add a new type - video 14

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,26 @@
-const Mutations = {};
+const Mutations = {
+  async createItem(parent, args, ctx, info) {
+    // todo: check if they are logged in
+
+    // ctx.db > it is how we access the db - returns a promise
+    const item = await ctx.db.mutation.createItem({
+      data: {
+        // title: args.title,
+        // description: args.description
+        ...args
+      }
+    }, info);
+
+    return item;
+  }
+  // createDog(parent, args, ctx, info) {
+  //   global.dogs = global.dogs || [];
+
+  //   // create a dog
+  //   const newDog = { name: args.name };
+  //   global.dogs.push(newDog);
+  //   return newDog;
+  // }
+};
 
 module.exports = Mutations;
--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,3 +1,14 @@
-const Query = {};
+const { forwardTo } = require('prisma-binding')
+
+const Query = {
+  // if using the exact same API on the client as on the server
+  items: forwardTo('db')
+
+  // if not using the exact same thing we need the below code
+  // async items(parent, args, ctx, info) {
+  //   const items = await ctx.db.query.items();
+  //   return items;
+  // }
+};
 
 module.exports = Query;
