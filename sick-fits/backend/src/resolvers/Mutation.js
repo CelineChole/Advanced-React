85ac5cc84f9a88f512f9f79205d20b34fa5b5034
celinechole <github@celinechole.com>
2019-12-16 09:57:32 -0500

    create user sign-in - video 24

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const mutations = {
   async createItem(parent, args, ctx, info) {
     // todo: check if they are logged in
@@ -29,10 +32,32 @@ const mutations = {
     const where = { id: args.id };
     // find the item
     const item = await ctx.db.query.item({ where }, `{ id title }`) 
-    // check if they own that item or have permission
+  // check if they own that item or have permission
     // todo
     // delete item
     return ctx.db.mutation.deleteItem({ where }, info)
+  },
+  async signup(parent, args, ctx, info) {
+    args.email = args.email.toLowerCase();
+    // hash their password
+    const password = await bcrypt.hash(args.password, 10)
+    // create the user in db
+    const user = await ctx.db.mutation.createUser({
+      data: {
+        ...args,
+        password,
+        permissions: { set: ['USER'] }
+      }
+    }, info);
+    // create the JWT token for them
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET)
+    // set the jwt as a cookie on the response
+    ctx.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365 // 1 year cookie
+    })
+    // return the user to the browser
+    return user
   }
 };
 
